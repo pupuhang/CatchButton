53e74341f701872f28cf09835922401afc4957bd
지웅 최 <sz2558@naver.com>
2026-03-12 10:56:56 +0900

    버튼에 마우스가 올라가면 버튼이 랜덤 좌표로 이동, 좌표 실시간 표

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            CatchButten = new Button();
+            SuspendLayout();
+            // 
+            // CatchButten
+            // 
+            CatchButten.Font = new Font("메이플스토리", 12F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            CatchButten.ForeColor = Color.Blue;
+            CatchButten.Location = new Point(340, 153);
+            CatchButten.Name = "CatchButten";
+            CatchButten.Size = new Size(131, 51);
+            CatchButten.TabIndex = 0;
+            CatchButten.Text = "나를 잡아봐";
+            CatchButten.UseVisualStyleBackColor = true;
+            CatchButten.MouseEnter += CatchButten_MouseEnter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(CatchButten);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button CatchButten;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,25 @@ namespace CatchButton
         {
             InitializeComponent();
         }
+
+        private void CatchButten_MouseEnter(object sender, EventArgs e)
+        {
+            // 1. 난수생성기준비
+            Random rd = new Random();
+
+            // 2. 가용영역계산 (버튼이폼테두리에걸리지않게보호)
+            int maxX = this.ClientSize.Width;
+            int maxY = this.ClientSize.Height;
+
+            // 3. 랜덤좌표추출 (0 ~ 최대가용치사이)
+            int nextX = rd.Next(0, maxX);
+            int nextY = rd.Next(0, maxY);
+
+            // 4. 랜덤 위치할당 (새로운Point 객체생성)
+            CatchButten.Location = new Point(nextX, nextY);
+
+            // 5. 시각적 피드백(폼 제목표시줄에 좌표출력)
+            this.Text = $"버튼위치: ({nextX}, {nextY})";
+        }
     }
 }
